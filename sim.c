41ed152a29cace6e4cf7859bf0f94518f1920657
fepz <franpaez+github@gmail.com>
2022-08-07 21:43:06 -0300

    Add functions to easily program scheduling events.

--- sim.c
+++ sim.c
@@ -21,10 +21,12 @@ struct event_list {
 };
 
 struct task {
+        int id;
         int wcet;
         int period;
         int deadline;
         int ret;
+        int instance;
 };
 
 struct task_item {
@@ -45,28 +47,28 @@ void insert_event(struct event_list* list, struct event* event);
 void insert_task_item(struct task_list* list, struct task_item* task_item);
 void remove_task_item(struct task_list* list, struct task_item* task_item);
 void print_list(struct event_list* list);
-void add_task_arrival(struct task* task, int time);
+void add_arrival(struct task* task, int time);
 void set_simulation_end(int time);
+void add_scheduling(int time);
+void add_terminate(int time);
 void schedule(struct event* e);
 
 void terminate(struct event* e) {
-        printf("%d\tE\t%d\n", e->time, task_list.head->prio);
-        // remove task item
         struct task_item *ti = task_list.head;
+        // remove task item
+        printf("%d\tT_%d_%d\tE\n", e->time, ti->task->id, ti->task->instance);
         remove_task_item(&task_list, ti);
         free(ti);
         // new scheduling event
-        insert_event(&list, new_event(e->time, SCHEDULING, schedule, NULL));
+        add_scheduling(e->time);
 }
 
 void schedule(struct event* e) {
         if (task_list.head) {
                 struct task* t = task_list.head->task;
-                printf("%d\tS\t%d\n", e->time, task_list.head->prio);
-                if (e->time + t->ret <= e->next->time) {
-                        insert_event(&list, new_event(e->time + t->ret, 
-                                                TERMINATE, terminate, (void*) NULL));
-                }
+                printf("%d\tT_%d_%d\tS\n", e->time, t->id, t->instance);
+                if (e->time + t->ret <= e->next->time)
+                        add_terminate(e->time + t->ret);
                 t->ret = t->ret - (e->next->time - e->time);
         } 
 }
@@ -75,10 +77,10 @@ int last_task_arrival = -1;
 
 void task_arrival(struct event* e) {
         struct task *t = (struct task*) e->data;
-        printf("%d\tA\t%d\n", e->time, t->period);
 
-        // Restore remanent execution time
+        // Restore remnant execution time
         t->ret = t->wcet;
+        t->instance = t->instance + 1;
 
         // Add task to ready list
         struct task_item* ti = (struct task_item*) malloc(sizeof(struct task_item));
@@ -90,11 +92,13 @@ void task_arrival(struct event* e) {
         // Call scheduling
         if (last_task_arrival < e->time) {
                 last_task_arrival = e->time;
-                insert_event(&list, new_event(e->time, SCHEDULING, schedule, NULL));
+                add_scheduling(e->time);
         }
 
         // Next arrival
-        add_task_arrival(t, e->time + t->period);
+        add_arrival(t, e->time + t->period);
+
+        printf("%d\tT_%d_%d\tA\n", e->time, t->id, t->instance);
 }
 
 void insert_task_item(struct task_list* list, struct task_item* task_item)
@@ -161,7 +165,7 @@ void sim(struct event_list* list)
                 if (head->type == END)
                         return;
                 head = head->next;
-        } while(head); 
+        } while(head);
 }
 
 void print_list(struct event_list* list)
@@ -173,9 +177,19 @@ void print_list(struct event_list* list)
         } while(head);
 }
 
-void add_task_arrival(struct task* task, int time)
+void add_arrival(struct task* task, int time)
 {
         insert_event(&list, new_event(time, ARRIVAL, task_arrival, (void*) task));
+}
+
+void add_scheduling(int time)
+{
+        insert_event(&list, new_event(time, SCHEDULING, schedule, (void*) NULL));
+}
+
+void add_terminate(int time)
+{
+        insert_event(&list, new_event(time, TERMINATE, terminate, (void*) NULL));
 }
 
 void set_simulation_end(int time)
@@ -183,32 +197,45 @@ void set_simulation_end(int time)
         insert_event(&list, new_event(time, END, terminate, (void*) 0));
 }
 
+void load_rts_from_file(FILE* f, int n, struct task_list *l)
+{
+
+}
+
 int main(int argc, char* argv[])
 {
         struct task *t1 = (struct task*) malloc(sizeof(struct task));
+        t1->id = 1;
         t1->wcet = 1;
         t1->period = 3;
         t1->deadline = 3;
         t1->ret = t1->wcet;
+        t1->instance = 0;
         struct task *t2 = (struct task*) malloc(sizeof(struct task));
+        t2->id = 2;
         t2->wcet = 1;
         t2->period = 4;
         t2->deadline = 4;
         t2->ret = t2->wcet;
+        t2->instance = 0;
         struct task *t3 = (struct task*) malloc(sizeof(struct task));
+        t3->id = 3;
         t3->wcet = 2;
         t3->period = 6;
         t3->deadline = 6;
         t3->ret = t3->wcet;
+        t3->instance = 0;
         struct task *t4 = (struct task*) malloc(sizeof(struct task));
+        t4->id = 4;
         t4->wcet = 1;
         t4->period = 12;
         t4->deadline = 12;
         t4->ret = t4->wcet;
-        add_task_arrival(t1, 0);
-        add_task_arrival(t2, 0);
-        add_task_arrival(t3, 0);
-        add_task_arrival(t4, 0);
+        t4->instance = 0;
+        add_arrival(t1, 0);
+        add_arrival(t2, 0);
+        add_arrival(t3, 0);
+        add_arrival(t4, 0);
         set_simulation_end(25);
         sim(&list);
         exit(EXIT_SUCCESS);
